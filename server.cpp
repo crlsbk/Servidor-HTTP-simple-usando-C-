619b873ce471b648863a6a31b0b5ea71f316250e
nicolas <juannnm05@gmail.com>
2025-09-17 11:49:41 -0600

    Mejoras a server.cpp y web ejemplo hecha.
    Se agregaron logs a server.cpp para hacer debug en caso de algún error. Agregado a esto, se agregaron 3 archivos web, un html, un js, y un css. Estos son los archivos que se mostraran cuando el programa se corra y el usuario ingrese a localhost:8080. Fueron agregados con propósito de demostrar el correcto funcionamiento del código, sin embargo no son necesarios para el servidor.

--- server.cpp
+++ server.cpp
@@ -19,7 +19,11 @@ string leerArchivo(const string &ruta)
 {
     ifstream archivo(ruta, ios::binary);
     if (!archivo.is_open())
+    {
+        cout << " No se pudo abrir el archivo: " << ruta << endl;
         return "";
+    }
+    cout << " Archivo abierto correctamente: " << ruta << endl;
     stringstream ss;
     ss << archivo.rdbuf();
     return ss.str();
@@ -42,73 +46,98 @@ string getTipoContenido(const string &ruta)
 
 string handleReq(const string &request)
 {
+    cout << " Nuevapetición recibida:\n"
+         << request << endl;
     size_t metodo_end = request.find(' ');
-    size_t path_end = request.find(' ', metodo_end + 1);
-    if (metodo_end == std::string::npos || path_end == std::string::npos)
+    size_t ruta_end = request.find(' ', metodo_end + 1);
+    if (metodo_end == string::npos || ruta_end == string::npos)
+    {
+        cout << " Petición mal hecha" << endl;
         return "HTTP/1.1 400 Bad Request\r\n\r\n";
+    }
 
-    std::string method = request.substr(0, metodo_end);
-    std::string url = request.substr(metodo_end + 1, path_end - metodo_end - 1);
+    string metodo = request.substr(0, metodo_end);
+    string url = request.substr(metodo_end + 1, ruta_end - metodo_end - 1);
 
-    if (method != "GET")
+    cout << " Método: " << metodo << ", URL: " << url << endl;
+
+    if (metodo != "GET")
+    {
+        cout << " Metodo no permitido: " << metodo << endl;
         return "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+    }
 
-    std::string filepath = "www" + (url == "/" ? "/index.html" : url);
-    std::string body = leerArchivo(filepath);
+    string rutaArch = "src/" + (url == "/" ? "/index.html" : url);
+    cout << " Ruta de archivo solicitada: " << rutaArch << endl;
+    string body = leerArchivo(rutaArch);
 
     if (body.empty())
+    {
+        cout << " Archivo no encontrado o vació: " << rutaArch << endl;
         return "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+    }
 
-    std::string contentType = getTipoContenido(filepath);
+    string contentType = getTipoContenido(rutaArch);
+    cout << " Tipo de contenido: " << contentType << ", Tamaño: " << body.size() << endl;
     return "HTTP/1.1 200 OK\r\nContent-Type: " + contentType +
-           "\r\nContent-Length: " + std::to_string(body.size()) + "\r\n\r\n" + body;
+           "\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n" + body;
 }
 
 int main()
 {
+    cout << " Iniciando servidor..." << endl;
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1)
     {
-        std::cerr << "Failed to create socket\n";
+        cerr << "Error al crear socket" << endl;
         return 1;
     }
 
-    sockaddr_in address{};
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+    sockaddr_in direccion{};
+    direccion.sin_family = AF_INET;
+    direccion.sin_addr.s_addr = INADDR_ANY;
+    direccion.sin_port = htons(8080);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    if (bind(server_fd, (struct sockaddr *)&direccion, sizeof(direccion)) < 0)
     {
-        std::cerr << "Bind failed\n";
+        cerr << "Error con bind" << endl;
         return 1;
     }
 
     if (listen(server_fd, 10) < 0)
     {
-        std::cerr << "Listen failed\n";
+        cerr << "Error con listen" << endl;
         return 1;
     }
-    std::cout << "Server running on http://localhost:8080\n";
+    cout << " Servidor corriendo en http://localhost:8080" << endl;
 
     while (true)
     {
-        int addrlen = sizeof(address);
-        int client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        int dirlen = sizeof(direccion);
+        int client_fd = accept(server_fd, (struct sockaddr *)&direccion, (socklen_t *)&dirlen);
         if (client_fd < 0)
         {
-            std::cerr << "Accept failed\n";
+            cerr << "Error con accept" << endl;
             continue;
         }
 
+        cout << " Cliente conectado" << endl;
         char buffer[4096] = {0};
-        read(client_fd, buffer, 4096);
+        ssize_t bytes_leidos = read(client_fd, buffer, 4096);
+        if (bytes_leidos <= 0)
+        {
+            cout << " No se leyeron datos del cliente" << endl;
+            close(client_fd);
+            continue;
+        }
 
-        std::string response = handleReq(buffer);
+        string response = handleReq(buffer);
         write(client_fd, response.c_str(), response.size());
+        cout << " Respuesta enviada, cerrando conexión con cliente" << endl;
         close(client_fd);
     }
 
     close(server_fd);
+    cout << " Servidor detenido" << endl;
     return 0;
 }
